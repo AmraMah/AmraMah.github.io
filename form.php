04a3d39921e64f39478eb70512db052706d10401
AmraMah <42281274+AmraMah@users.noreply.github.com>
2020-06-24 22:31:44 +0200

    Create form.php

---
+++ form.php
@@ -0,0 +1,13 @@
+<?php
+
+if(isset($_POST['submit'])) {
+$name=$_POST['name'];
+$from=$_POST['email'];
+$message=$_POST['message'];
+$mailTo="amra10mujic@gmail.com";
+$headers="From: ".$from;
+$text="You have received an email from ".$name."\n\n".$message;
+
+mail($mailTo, $text, $headers);
+
+}
